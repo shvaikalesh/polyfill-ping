85abc4cf92ed459116fe065718761d7303d712db
Aleksej Shvajka <shvaikalesh@gmail.com>
2016-03-09 10:56:13 +0200

    code style changes

--- ping.js
+++ ping.js
@@ -4,65 +4,54 @@
 
     if (/webkit/i.test(navigator.userAgent)) return
 
-    var reflect = require("reflect-attributes")
+    var tls = "https:" == location.protocol
+    var reflect = require("reflect-attribute")
         reflect("ping", [ "a", "area" ])
 
+    function resolve(href)
     {
-          , secure = location.protocol == "https:"
+        if (typeof href == "string")
+            try { return new URL(href, location) }
+            finally { }
 
-        function resolve(url)
+        return ""
+    }
+
+    document.addEventListener("click", function(event)
+    {
+        if (event.defaultPrevented) return
+
+        var $link = event.target.closest("a, area")
+        if ($link == null) return
+
+        var href = resolve($link.getAttribute("href"))
+        if (href == "") return
+
+        var ping = $link.ping
+            .trim()
+            .split(/\s+/)
+            .map(resolve)
+            .filter(String)
+
+        ping.forEach(function(url)
         {
-            if (typeof url == "string")
+            var options =
             {
-                try { return new URL(url, location) }
-                finally { }
+                method: "POST",
+                headers:
+                {
+                    "Ping-From": location,
+                    "Ping-To": href
+                },
+                referrer: "no-referrer",
+                body: "PING"
             }
 
-            return ""
-        }
-
-        document.addEventListener("click", function(event)
-        {
-            if (event.defaultPrevented) return
-
-            var $link = event.target.closest("a, area")
-            if ($link == null) return
-
-            var href = resolve($link.getAttribute("href"))
-            if (href == "") return
-
-            var ping = $link.ping
-                .trim()
-                .split(/\s+/)
-                .map(resolve)
-                .filter(String)
-
-            ping.forEach(function(url)
-            {
-                var options =
-                {
-                    method: "POST",
-                    headers:
-                    {
-                        "Ping-From": location,
-                        "Ping-To": href
-                    },
-                    referrer: "no-referrer",
-                    body: "PING"
-                }
-
-                if (url.origin != location.origin)
-                    if (secure) options.referrer = location
-                    else delete options.headers["Ping-From"]
-
-            })
-        })
-
-        {
-            {
-                {
-                },
-
+            if (url.origin != location.origin)
+                if (tls) options.referrer = location
+                else delete options.headers["Ping-From"]
 
             fetch(url, options)
+        })
+    })
 })()
